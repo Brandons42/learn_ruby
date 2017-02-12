0e2df42eeef4ec881d5fbde9ed734bd37ef8e8bb
Brandon <brandonbsuen@gmail.com>
2017-02-11 17:07:48 -0800

    All challenges completed

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,43 @@
-#write your code here
+def echo phrase
+  phrase
+end
+
+def shout phrase
+  phrase.upcase
+end
+
+def repeat(*args)
+  if args.length == 1
+    return "#{args[0]} #{args[0]}"
+  else
+    string = args[0]
+    args[1] -= 1
+    args[1].times do
+      string += " #{args[0]}"
+    end
+    string
+  end
+end
+
+def start_of_word(word, number)
+  word[0...number]
+end
+
+def first_word(phrase)
+  phrase = phrase.split
+  phrase[0]
+end
+
+def titleize(phrase)
+  phrase = phrase.split
+  phrase.map! do |q|
+    if q.length > 4
+      q.capitalize
+    else
+      q.downcase
+    end
+  end
+  phrase[0] = phrase[0].capitalize
+  phrase[phrase.length - 1] = phrase[phrase.length - 1].capitalize
+  phrase.join(" ")
+end
