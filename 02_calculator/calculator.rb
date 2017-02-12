8871488a0a9e65e935a618ba387ff9bcd2073bbe
Brandon <brandonbsuen@gmail.com>
2017-02-11 17:04:28 -0800

    Challenges completed

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,27 @@
-#write your code here
+def add first, second
+  first + second
+end
+
+def subtract first, second
+  first - second
+end
+
+def sum array
+  if array.length == 0 then
+    0
+  else
+    total = 0
+    array.each do |x|
+      total += x
+    end
+  total
+  end
+end
+
+def multiply array
+  total = 1
+  array.each do |x|
+    total *= x
+  end
+  total
+end
